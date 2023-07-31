08c9a02a033fb02363e130fe7e3963995da6eae9
Alen Gomez <81427082+alenpragma@users.noreply.github.com>
2023-07-31 22:44:35 +0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
         {mobile &&
           <div className="xs:block w-full md:w-3/4 flex flex-col md:flex-row md:items-center gap-x-2 text-end  md:justify-end">
           <ul className='flex flex-col md:flex-row gap-y-[15px] md:gap-x-3 lg:gap-x-6 font-cabin font-normal text-sm uppercase text-center'>
-            <li className=''><a href="https://www.mindchain.info/">Mindchain Coin</a></li>
+            <li className=''><a href="https://www.mindchain.info">Mindchain Coin</a></li>
             <li className='' ><a href="#">pMIND</a></li>
             <li className='' ><a href="#">Roadmap</a></li>
             <li className='' ><a href="#">News</a></li>
