5cc74b394241516bbcd7e225669119e398f520c7
Alen Gomez <81427082+alenpragma@users.noreply.github.com>
2023-08-19 23:14:40 +0600

    Update Media.jsx

--- src/components/Media.jsx
+++ src/components/Media.jsx
@@ -22,7 +22,7 @@ const Media = () => {
             <div className="w-full">
                 <div className="text-center w-3/4 mx-auto">
                     <h2 className='font-cabin font-semibold text-[32px] relative before:absolute before:w-[75px] before:h-[3px] before:bg-primary before:content-[""] before:top-[-34%] before:left-[50%] before:translate-x-[-50%] before:translate-y-[-50%] before:z-10'> Media & Partners </h2>
-                    <p className='font-cabin font-normal text-[18px] pt-[30px] md:px-[100px] pb-[50px] mx-auto'> Viderer malorum sadipscing cum ei. Eu impetus perfecto sit, no sea labore detraxit. Primis mediocrem necessitatibus an vis. Ut sea pertinax perpetua, eruditi volumus quaestio ex mel, has ei tota homero. </p>
+                    <p className='font-cabin font-normal text-[18px] pt-[30px] md:px-[100px] pb-[50px] mx-auto'> Explore our blog for insightful articles, personal reflections and ideas that inspire action on the topics you care about.</p>
                 </div>
 
                 <div className="w-full mx-auto">
@@ -31,15 +31,13 @@ const Media = () => {
                             <img className='w-[30rem]' src={post1} alt="post1" />
                         </div>
                         <div className="">
-                            <h2 className='font-cabin font-medium text-[18px]'>Blockchain for dummies <span className='font-cabin font-light text-[16px] pl-[20px] md:pl-[40px] relative before:absolute before:w-[3px] before:h-[20px] before:bg-primary before:content-[""] before:bottom-[-50%] before:left-[5%] md:before:left-[20%]  before:translate-x-[-50%] before:translate-y-[-50%] before:z-10'>15.04.2021</span></h2>
-                            <p className='py-[20px]'>Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan
-                            mıgır metinlerdir. Lorem Ipsum, adı bilinmeyen bir
-                            matbaacının bir hurufat numune kitabı...
+                            <h2 className='font-cabin font-medium text-[18px]'>How You Can Get Free MIND on MSC Testnet <span className='font-cabin font-light text-[16px] pl-[20px] md:pl-[40px] relative before:absolute before:w-[3px] before:h-[20px] before:bg-primary before:content-[""] before:bottom-[-50%] before:left-[5%] md:before:left-[20%]  before:translate-x-[-50%] before:translate-y-[-50%] before:z-10'>June 8, 2023</span></h2>
+                            <p className='py-[20px]'>The best way to get free MIND testnet tokens is from the official Mind Smart Chain Faucet (MIND Faucet). With this method, all users need to do is visit the website and enter their MSC testnet address to receive 1 free MIND testnet coin.
                             </p>
                             <div className="flex item-center justify-between">
                                 <div className="flex items-center gap-3">
                                     <img className='rounded-full w-[40px]' src={post_team1} alt="post_team1" />
-                                    <h3 className='font-cabin font-normal text-[16px]'>by John Cambell</h3>
+                                    <h3 className='font-cabin font-normal text-[16px]'>by Alice Tanja</h3>
                                 </div>
                                 <div className="icon flex gap-2">
                                     <div className="flex gap-1 items-center">
@@ -59,15 +57,13 @@ const Media = () => {
                             <img className='w-[30rem]' src={post2} alt="post2" />
                         </div>
                         <div className="">
-                            <h2 className='font-cabin font-medium text-[18px]'> The Mind chain algorythm <span className='font-cabin font-light text-[16px] pl-[20px] md:pl-[40px] relative before:absolute before:w-[3px] before:h-[20px] before:bg-primary before:content-[""] before:bottom-[-50%] before:left-[5%] md:before:left-[20%]  before:translate-x-[-50%] before:translate-y-[-50%] before:z-10'>15.04.2021</span></h2>
-                            <p className='py-[20px]'>Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan
-                            mıgır metinlerdir. Lorem Ipsum, adı bilinmeyen bir
-                            matbaacının bir hurufat numune kitabı...
+                            <h2 className='font-cabin font-medium text-[18px]'> Explaining The Mindchain Ecosystem <span className='font-cabin font-light text-[16px] pl-[20px] md:pl-[40px] relative before:absolute before:w-[3px] before:h-[20px] before:bg-primary before:content-[""] before:bottom-[-50%] before:left-[5%] md:before:left-[20%]  before:translate-x-[-50%] before:translate-y-[-50%] before:z-10'>January 19, 2023</span></h2>
+                            <p className='py-[20px]'>Mindchain is a fast, high-throughput open-source smart contract platform for digital assets and dApps. It offers unmatched speed, security, and reliability. Enjoy almost instant transactions and extremely low fees.
                             </p>
                             <div className="flex item-center justify-between">
                                 <div className="flex items-center gap-3">
                                     <img className='rounded-full w-[40px]' src={post_team2} alt="post_team1" />
-                                    <h3 className='font-cabin font-normal text-[16px]'>by Joshua Stevens</h3>
+                                    <h3 className='font-cabin font-normal text-[16px]'>by John Stevens</h3>
                                 </div>
                                 <div className="icon flex gap-2">
                                     <div className="flex gap-1 items-center">
@@ -87,10 +83,8 @@ const Media = () => {
                             <img className='w-[30rem]' src={post1} alt="post1" />
                         </div>
                         <div className="">
-                            <h2 className='font-cabin font-medium text-[18px]'>What is Crypto mining ?<span className='font-cabin font-light text-[16px] pl-[20px] md:pl-[40px] relative before:absolute before:w-[3px] before:h-[20px] before:bg-primary before:content-[""] before:bottom-[-50%] before:left-[5%] md:before:left-[20%]  before:translate-x-[-50%] before:translate-y-[-50%] before:z-10'>15.04.2021</span></h2>
-                            <p className='py-[20px]'>Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan
-                            mıgır metinlerdir. Lorem Ipsum, adı bilinmeyen bir
-                            matbaacının bir hurufat numune kitabı...
+                            <h2 className='font-cabin font-medium text-[18px]'>How to become Mindchain consultant?<span className='font-cabin font-light text-[16px] pl-[20px] md:pl-[40px] relative before:absolute before:w-[3px] before:h-[20px] before:bg-primary before:content-[""] before:bottom-[-50%] before:left-[5%] md:before:left-[20%]  before:translate-x-[-50%] before:translate-y-[-50%] before:z-10'>February 14, 2023</span></h2>
+                            <p className='py-[20px]'>A cryptocurrency consultant is a professional who guides clients to successful, compliant cryptocurrency transactions and practices across accounting and tax functions. They protect their clients from illegal and unethical crypto practices.
                             </p>
                             <div className="flex item-center justify-between">
                                 <div className="flex items-center gap-3">
@@ -112,7 +106,7 @@ const Media = () => {
                     </div>
 
                     <div className="text-center pt-[50px]">
-                        <button className=' bg-gradient-to-r from-[#FFBF5E] bg-primary py-2 px-8 uppercase  text-white rounded hover:bg-[#dea029]'>More Articles</button>
+                        <button className=' bg-gradient-to-r from-[#FFBF5E] bg-primary py-2 px-8 uppercase  text-white rounded hover:bg-[#dea029]'><a href="https://academy.mindchain.info/"></a>More Articles</button>a></button>
                     </div>
                 </div>
 
